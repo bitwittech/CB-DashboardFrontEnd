902638340627131332ecc6ae0c449750b3bfe462
Yashwant <yashwantsahu3002@gmail.com>
2022-05-21 15:42:40 +0530

    User Tracking Frontend Completed

--- src/components/UserTracking.js
+++ src/components/UserTracking.js
@@ -4,194 +4,435 @@ import {
   FormControl,
   MenuItem,
   Typography,
-  TextField,Backdrop,
+  TextField,
+  Backdrop,
   InputLabel,
   Select,
   Grid,
-  Button,Modal,Fade,
+  LinearProgress,
+  Button,
+  Modal,
+  Fade,
 } from "@mui/material";
 import {
-    ResponsiveContainer,
-    Cell,
-    Legend,
-    Tooltip,
-    PieChart,
-    Pie,
-  } from "recharts";
-import { DataGrid } from "@mui/x-data-grid";
+  ResponsiveContainer,
+  Cell,
+  Legend,
+  Tooltip,
+  PieChart,
+  Pie,
+} from "recharts";
+import {
+  DataGrid,
+  gridPageCountSelector,
+  gridPageSelector,
+  useGridApiContext,
+  useGridSelector,
+} from "@mui/x-data-grid";
 import { OpenBox, Notify } from "../App";
+import Pagination from "@mui/material/Pagination";
 
-import { listTrackData } from '../services/service'
+import {
+  listTrackData,
+  listEnrollTrack,
+  listCardTrack,
+  listSearchTrack,
+} from "../services/service";
 
-import '../assets/custom/css/category.css'
-import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import "../assets/custom/css/category.css";
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 import "../assets/custom/css/dashboard.css";
 
-import RouteIcon from '@mui/icons-material/Route';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
-import SearchIcon from '@mui/icons-material/Search';
+import RouteIcon from "@mui/icons-material/Route";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function UserTracking() {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 800,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
 
-
-
-  const [category, setCategory] = useState("");
   const [search, setSearch] = useState("");
+  const [gridTitle, setgridTitle] = useState("Click Over The Track Cards");
 
   const SideBox = useContext(OpenBox);
   const despatchAlert = useContext(Notify);
 
-
-  const [Row, setRows] = useState()
-
-
-// function for get cetegory list
-
-  useEffect(() => {
-    listTrackData()
-      .then((data) => {
-        console.log(data)
-
-        setRows(data.data.map((row) => {
-
-          return ({
-            id: row._id,
-            user_email: row.user_email,
-            time_stamp: row.time_stamp,
-            page_time_span: row.page_time_span
-          })
-        }))
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, []);
-
   const [open, setOpen] = React.useState(false);
-  const handleOpen = () => {return setOpen(true)};
+  const handleOpen = () => {
+    return setOpen(true);
+  };
   const handleClose = () => setOpen(false);
 
+  const [Data, setData] = useState([]);
+  const [columns, setCol] = useState([]);
+  const [Row, setRows] = useState();
 
-  const columns = [
-    {
-      field: "id",
-      headerName: "ID",
-      width: 100
-    },
-    {
-      field: 'user_email',
-      headerName: 'User Email',
-      width: 200,
-    },
-    {
-      field: "time_stamp",
-      headerName: "Event Time",
-      width: 200,
-    },
-    {
-      field: "page_time_span",
-      headerName: "Path",
-      width: 500,
-    }
-
+  var colors = [
+    "#FF6633",
+    "#FFB399",
+    "#FF33FF",
+    "#FFFF99",
+    "#00B3E6",
+    "#E6B333",
+    "#3366E6",
+    "#999966",
+    "#99FF99",
+    "#B34D4D",
+    "#80B300",
+    "#809900",
+    "#E6B3B3",
+    "#6680B3",
+    "#66991A",
+    "#FF99E6",
+    "#CCFF1A",
+    "#FF1A66",
+    "#E6331A",
+    "#33FFCC",
+    "#66994D",
+    "#B366CC",
+    "#4D8000",
+    "#B33300",
+    "#CC80CC",
+    "#66664D",
+    "#991AFF",
+    "#E666FF",
+    "#4DB3FF",
+    "#1AB399",
+    "#E666B3",
+    "#33991A",
+    "#CC9999",
+    "#B3B31A",
+    "#00E680",
+    "#4D8066",
+    "#809980",
+    "#E6FF80",
+    "#1AFF33",
+    "#999933",
+    "#FF3380",
+    "#CCCC00",
+    "#66E64D",
+    "#4D80CC",
+    "#9900B3",
+    "#E64D66",
+    "#4DB380",
+    "#FF4D4D",
+    "#99E6E6",
+    "#6666FF",
   ];
 
-  const [Data,setData] = useState([
-    {
-      name: "Group A",
-      value: 400,
-    },
-    {
-      name: "Group B",
-      value: 300,
-    },
-    {
-      name: "Group C",
-      value: 300,
-    },
-    {
-      name: "Group D",
-      value: 200,
-    },
-    {
-      name: "Group E",
-      value: 278,
-    },
-    {
-      name: "Group F",
-      value: 189,
-    },
-  ])
-
-  var colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
-		  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-		  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
-		  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-		  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
-		  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-		  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
-		  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-		  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
-		  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
-
-
-          const style = {
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            width: 800,
-            bgcolor: 'background.paper',
-            border: '2px solid #000',
-            boxShadow: 24,
-            p: 4,
-          };
-          
-
- const handleOnCellClick = (data) =>{
-
+  const handleOnCellClick = (data) => {
     console.log(data);
 
-    let DataArr = []
+    let DataArr = [];
 
-    JSON.parse(data.row.page_time_span)
-    .map((row)=>{
-        DataArr.push({name : row.path,value : row.time})
-    })
+    JSON.parse(data.row.page_time_span).map((row) => {
+      DataArr.push({ name: row.path, value: row.time });
+    });
 
-    setData(DataArr)
+    setData(DataArr);
 
-    handleOpen()
+    handleOpen();
+  };
 
- }
+  const col_row_change = (e) => {
+    console.log(e);
 
-  const handelSearch = (e)=>{
-    console.log(e.target.value)
-    setSearch(e.target.value)
-  }
+    setgridTitle("Loading...");
 
+    switch (e) {
+      case "path":
+        setCol([
+          {
+            field: "id",
+            headerName: "ID",
+            width: 100,
+          },
+          {
+            field: "user_email",
+            headerName: "User Email",
+            width: 200,
+          },
+          {
+            field: "time_stamp",
+            headerName: "Event Time",
+            width: 200,
+          },
+          {
+            field: "page_time_span",
+            headerName: "Path",
+            width: 500,
+          },
+        ]);
+
+        listTrackData()
+          .then((data) => {
+            console.log(data);
+
+            setRows(
+              data.data.map((row) => {
+                setgridTitle("User Path");
+
+                return {
+                  id: row._id,
+                  user_email: row.user_email,
+                  time_stamp: row.time_stamp,
+                  page_time_span: row.page_time_span,
+                };
+              })
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        break;
+
+      case "search":
+        setCol([
+          {
+            field: "id",
+            headerName: "ID",
+            width: 100,
+          },
+          {
+            field: "user_email",
+            headerName: "User Email",
+            width: 200,
+          },
+          {
+            field: "event_time",
+            headerName: "Event Time",
+            width: 200,
+          },
+          {
+            field: "search_query",
+            headerName: "Query",
+            width: 500,
+          },
+        ]);
+
+        listSearchTrack()
+          .then((data) => {
+            console.log(data);
+            setRows(
+              data.data.map((row) => {
+                setgridTitle("Search Track");
+
+                return {
+                  id: row._id,
+                  user_email: row.user_email,
+                  event_time: row.time_stamp,
+                  search_query: row.search_query,
+                };
+              })
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        break;
+
+      case "card":
+        setCol([
+          {
+            field: "id",
+            headerName: "ID",
+            width: 100,
+          },
+          {
+            field: "user_email",
+            headerName: "User Email",
+            width: 200,
+          },
+          {
+            field: "event_time",
+            headerName: "Event Time",
+            width: 200,
+          },
+          {
+            field: "card_title",
+            headerName: "Course Title",
+            width: 500,
+          },
+          {
+            field: "card_uuid",
+            headerName: "Card UUID",
+            width: 200,
+          },
+          {
+            field: "provider",
+            headerName: "Provider",
+            width: 200,
+          },
+        ]);
+
+        listCardTrack()
+          .then((data) => {
+            console.log(data);
+            setRows(
+              data.data.map((row) => {
+                setgridTitle("Card Track");
+
+                return {
+                  id: row._id,
+                  user_email: row.user_email,
+                  event_time: row.event_time,
+                  card_title: row.card_title,
+                  card_uuid: row.card_uuid,
+                  provider: row.provider,
+                };
+              })
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        break;
+
+      case "enroll":
+        setCol([
+          {
+            field: "id",
+            headerName: "ID",
+            width: 100,
+          },
+          {
+            field: "user_email",
+            headerName: "User Email",
+            width: 200,
+          },
+          {
+            field: "event_time",
+            headerName: "Event Time",
+            width: 200,
+          },
+          {
+            field: "card_title",
+            headerName: "Course Title",
+            width: 500,
+          },
+          {
+            field: "card_uuid",
+            headerName: "Card UUID",
+            width: 200,
+          },
+          {
+            field: "provider",
+            headerName: "Provider",
+            width: 200,
+          },
+        ]);
+
+        listEnrollTrack()
+          .then((data) => {
+            console.log(data);
+            setRows(
+              data.data.map((row) => {
+                setgridTitle("Enroll Track");
+
+                return {
+                  id: row._id,
+                  user_email: row.user_email,
+                  event_time: row.event_time,
+                  card_title: row.card_title,
+                  card_uuid: row.card_uuid,
+                  provider: row.provider,
+                };
+              })
+            );
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+
+        break;
+      default:
+        setCol([
+          {
+            field: "id",
+            headerName: "ID",
+            width: 100,
+          },
+          {
+            field: "user_email",
+            headerName: "User Email",
+            width: 200,
+          },
+          {
+            field: "time_stamp",
+            headerName: "Event Time",
+            width: 200,
+          },
+          {
+            field: "page_time_span",
+            headerName: "Path",
+            width: 500,
+          },
+        ]);
+        break;
+    }
+  };
+
+  const handelSearch = (e) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  };
 
   function DataGridView() {
+    function CustomPagination() {
+      const apiRef = useGridApiContext();
+      const page = useGridSelector(apiRef, gridPageSelector);
+      const pageCount = useGridSelector(apiRef, gridPageCountSelector);
+
+      return (
+        <Pagination
+          color="primary"
+          count={pageCount}
+          page={page + 1}
+          onChange={(event, value) => apiRef.current.setPage(value - 1)}
+        />
+      );
+    }
+
     return (
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
+          pagination
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+          components={{
+            Pagination: CustomPagination,
+            LoadingOverlay: LinearProgress,
+          }}
+          loading={gridTitle === "Loading..." ? true : false}
           filterModel={{
-            items: [{ columnField: 'user_email', operatorValue: 'contains', value: `${search}` }],
+            items: [
+              {
+                columnField: "user_email",
+                operatorValue: "contains",
+                value: `${search}`,
+              },
+            ],
           }}
           rows={Row}
           columns={columns}
-          pageSize={5}
-          rowsPerPageOptions={[5]}
           onCellClick={handleOnCellClick}
         />
       </div>
     );
   }
-
-  const handleChangeCat = (event) => {
-    setCategory(event.target.value);
-  };
 
   return (
     <>
@@ -225,7 +466,6 @@ export default function UserTracking() {
           />
         </Grid>
 
-
         <Grid xs={12} md={3}>
           <Button
             onClick={() => {
@@ -243,13 +483,13 @@ export default function UserTracking() {
 
       {/* Section 1 ends  */}
 
-
       <br></br>
 
       <br></br>
 
       <Grid container className="overviewContainer" spacing={1.2}>
         <Grid
+          onClick={() => col_row_change("path")}
           item
           xs={12}
           md={2.8}
@@ -267,6 +507,7 @@ export default function UserTracking() {
         </Grid>
 
         <Grid
+          onClick={() => col_row_change("card")}
           item
           xs={12}
           md={2.8}
@@ -284,6 +525,7 @@ export default function UserTracking() {
         </Grid>
 
         <Grid
+          onClick={() => col_row_change("enroll")}
           item
           xs={12}
           md={2.8}
@@ -301,6 +543,7 @@ export default function UserTracking() {
         </Grid>
 
         <Grid
+          onClick={() => col_row_change("search")}
           item
           xs={12}
           md={2.8}
@@ -318,13 +561,13 @@ export default function UserTracking() {
         </Grid>
       </Grid>
 
-      {/* <br></br> */}
+      <br></br>
 
       {/* data grid  */}
 
       <Grid container scaping={2} className="overviewContainer">
         <Grid item p={2} xs={12} sx={{ boxShadow: 2, borderRadius: 5 }}>
-          <Typography variant="h6"> Category List </Typography>
+          <Typography variant="h6"> {gridTitle}</Typography>
           <br></br>
           {DataGridView()}
         </Grid>
@@ -345,55 +588,38 @@ export default function UserTracking() {
       >
         <Fade in={open}>
           <Box sx={style}>
+            {/* char view  */}
 
-                                    {/* char view  */}
-
-                    <Grid container scaping={2} className="overviewContainer">
-                    
-                    <Grid
-                    item
-                    p={2}
-                    xs={12}
-                    sx={{ boxShadow: 2, borderRadius: 5 }}
+            <Grid container scaping={2} className="overviewContainer">
+              <Grid item p={2} xs={12} sx={{ boxShadow: 2, borderRadius: 5 }}>
+                <Typography variant="h6"> User Path </Typography>
+                <br></br>
+                <ResponsiveContainer width="95%" height="85%">
+                  <PieChart className="chart">
+                    <Tooltip />
+                    <Legend />
+                    <Pie
+                      data={Data}
+                      dataKey="value"
+                      nameKey="name"
+                      cx="50%"
+                      cy="50%"
+                      outerRadius={80}
+                      fill="#8884d8"
                     >
-                    <Typography variant="h6"> User Path </Typography>
-                    <br></br>
-                    <ResponsiveContainer width="95%" height="85%">
-                        <PieChart className="chart">
-                        <Tooltip />
-                        <Legend />
-                        <Pie
-                            data={Data}
-                            dataKey="value"
-                            nameKey="name"
-                            cx="50%"
-                            cy="50%"
-                            outerRadius={80}
-                            fill="#8884d8"
-                        >
+                      {Data.map((entry, index) => (
+                        <Cell key={`cell-${index}`} fill={colors[index]} />
+                      ))}
+                    </Pie>
+                  </PieChart>
+                </ResponsiveContainer>
+              </Grid>
+            </Grid>
 
-                {
-                Data.map((entry, index) => (
-                    <Cell key={`cell-${index}`} fill={colors[index]}/>
-                ))
-                }
-
-                        </Pie>
-                        </PieChart>
-                    </ResponsiveContainer>
-                    </Grid>
-                </Grid>
-
-                {/* char view ends */}
-
-            
+            {/* char view ends */}
           </Box>
         </Fade>
       </Modal>
-
-
-
-
     </>
   );
 }
